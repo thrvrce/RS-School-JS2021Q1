8b59172c2c76ac8da5ec7b6a5e28f407561a8122
Victor Avdeev <avdeevvictor95@gmail.com>
2021-04-22 06:19:02 +0300

    fead: add zoos panda adaptive

--- online-zoo/pages/zoos/cam.js
+++ online-zoo/pages/zoos/cam.js
@@ -6,18 +6,26 @@ const handleResize = () => {
   // двигат сайдбар в зависимости от положения эдемента с выбором видео
   const windowWidth = document.documentElement.offsetWidth;
   cameraSelector = document.querySelector('.animal-camera__video-selector');
-  let sidebarLeft = '';
-  if (windowWidth >= 1600) {
-    sidebarLeft = '';
-  }
-  if (windowWidth < 1600) {
-    sidebarLeft = cameraSelector.getBoundingClientRect().x - 72 - 90 ;
-  }
-  if (windowWidth < 1000) {
-    sidebarLeft = cameraSelector.getBoundingClientRect().x - 72 - 33;
-  }
 
-  sideBar.style.left = sidebarLeft + 'px';
+    let sidebarLeft = '';
+    if (windowWidth >= 1600) {
+      sidebarLeft = '';
+    }
+    if (windowWidth < 1600) {
+      sidebarLeft = cameraSelector.getBoundingClientRect().x - 72 - 90 + 'px';
+    }
+    if (windowWidth < 1000) {
+      sidebarLeft = cameraSelector.getBoundingClientRect().x - 72 - 33 + 'px';
+    }
+    if (windowWidth < 640) {
+      sidebarLeft = cameraSelector.getBoundingClientRect().x - 72 - 41 + 'px';
+    }
+
+    sideBar.style.left = sidebarLeft ;
+
+
+
+
 }
 
 window.addEventListener('load', handleResize)
